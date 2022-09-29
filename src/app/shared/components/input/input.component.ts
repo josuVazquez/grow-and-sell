f568aa14ebd528fdb1d67a0044d508cce580b903
amutarbo-dev <amutarbo.dev@gmail.com>
2022-09-30 01:10:57 +0200

    input with type password

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -8,6 +8,7 @@ import { Component, Input } from '@angular/core';
 export class InputComponent {
   @Input() name: string;
   @Input() placeholder: string;
+  @Input() type: string;
 
   constructor() {}
 }
