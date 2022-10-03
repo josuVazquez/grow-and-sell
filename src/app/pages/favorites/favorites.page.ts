289c810b834d16d61b8b4db1d056b25c8af8a9a1
amutarbo-dev <amutarbo.dev@gmail.com>
2022-10-03 19:07:35 +0200

    cards

--- src/app/pages/favorites/favorites.page.ts
+++ src/app/pages/favorites/favorites.page.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favorites.page.scss'],
 })
 export class FavoritesPage implements OnInit {
+  cardExample = [
+    {
+      id: 1,
+      img: 'assets/products/patata-propiedades.jpg',
+      title: 'Patatas',
+      subtitle: '5 kilos patatas',
+      description: '5 kilos patatas por 3 euro, no negocio',
+    },
+    {
+      id: 2,
+      img: 'assets/products/patata-propiedades.jpg',
+      title: 'Patatas',
+      subtitle: '5 kilos patatas',
+      description: '5 kilos patatas por 3 euro, no negocio',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
+  deleteItem() {}
 }
--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
-  mobile: true;
+  @Input() img: string;
+  @Input() title: string;
+  @Input() subtitle: string;
+  @Input() description: string;
 
   constructor() {}
 
   ngOnInit() {}
+
+  // TODO: pipe translate
 }
