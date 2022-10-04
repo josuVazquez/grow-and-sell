16ef33b68fdec818163c5b2630f317f218db6f40
amutarbo-dev <amutarbo.dev@gmail.com>
2022-10-04 18:42:20 +0200

    data back profile

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,13 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
   data = {
+    img: '',
     name: 'Nombre Apellido Apellido',
     email: 'example@example.com',
     phone: '+34 12393123',
     rating: '3.0 / 5.0',
   };
 
-  constructor() {}
+  constructor(private userService: UserService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // faltaria un rating y un numero de telefono opcional (?)
+    this.userService._getUser().subscribe((dt) => {
+      this.data = {
+        ...this.data,
+        name: dt.displayName,
+        email: dt.email,
+        img: dt.photoURL,
+      };
+    });
+  }
 }
