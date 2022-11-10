cfeea94f161a88429a8426385fd2cc50b685cdf4
amutarbo-dev <amutarbo.dev@gmail.com>
2022-11-10 21:04:46 +0100

    user changes profile
    das

--- src/app/shared/models/user.model.ts
+++ src/app/shared/models/user.model.ts
@@ -1,21 +1,21 @@
 export class User {
-    uid: string;
-    email: string;
-    name: string;
-    photoURL: string;
-    phone: string;
-    rating: number;
-    lat: string;
-    lng: string;
+  uid: string;
+  email: string;
+  displayName: string;
+  photoURL: string;
+  phone: string;
+  rating: number;
+  lat: string;
+  lng: string;
 
-    constructor(data: Partial<User | any> = {}) {
-        this.uid = data.uid;
-        this.email = data.email;
-        this.name = data.displayName;
-        this.phone = data.phone;
-        this.rating = data.rating;
-        this.photoURL = data.photoURL;
-        this.lat = data.lat;
-        this.lng = data.lng;
-    }
+  constructor(data: Partial<User | any> = {}) {
+    this.uid = data.uid;
+    this.email = data.email;
+    this.displayName = data.displayName;
+    this.phone = data.phone;
+    this.rating = data.rating;
+    this.photoURL = data.photoURL;
+    this.lat = data.lat;
+    this.lng = data.lng;
+  }
 }
